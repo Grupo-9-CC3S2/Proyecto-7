b83e9057407ce86e0c8b61cc1f87a729458cb583
scptx0 <134897241+scptx0@users.noreply.github.com>
2025-06-08 15:53:04 -0500

    feat(tf): (Issue #3) agregar iac/main.tf con tres recursos null_resource dummy

---
+++ iac/main.tf
@@ -0,0 +1,17 @@
+resource "null_resource" "service_1" {
+    provisioner "local-exec" {
+        command = "echo 'Ejemplo de servicio 1' > service_1.txt"
+    }
+}
+
+resource "null_resource" "service_2" {
+    provisioner "local-exec" {
+        command = "echo 'Ejemplo de servicio 2' > service_2.txt"
+    }
+}
+
+resource "null_resource" "service_3" {
+    provisioner "local-exec" {
+        command = "echo 'Ejemplo de servicio 3' > service_3.txt"
+    }
+}
