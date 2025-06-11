c93a6279e6e76730e8c77bb61a8d745a8d3c7997
Andre <andresan0808@gmail.com>
2025-06-10 22:39:01 -0500

    fix(script): (Issue #6) ajustar simulate_drift.sh para renombrar el recurso service_q y poder forzar el drift

--- scripts/simulate_drift.sh
+++ scripts/simulate_drift.sh
@@ -3,15 +3,16 @@
 # timestamp para distinguir cada ejecución
 timestamp=$(date +%Y-%m-%d_%H-%M-%S)
 
-# modificar un valor en iac/main.tf para forzar drift
-sed -i "s/always_run = .*/always_run = \"$timestamp\"/" ../iac/main.tf
+#Para forzar el drfit cambiamos el nombre del recurso "service_1" a "service_1_drift"
+# en ../iac/main.tf para que terraform plan detecte una desviación
+sed -i 's/resource "null_resource" "service_1"/resource "null_resource" "service_1_drift"/' ../iac/main.tf
 
-# ejecutar terraform plan y guardar log
+# ejecutamos terraform plan y guardamos en log
 mkdir -p ../logs
 
-# 4. Entrar en la carpeta iac, correr plan, muestrar y guardar la salida en ../logs
 cd ../iac
 
+#corremos plan, luego mostrar y guardar la salida en ../logs
 terraform init -input=false        # ← inicializa sin pedir interacción
 terraform plan | tee ../logs/drift_"$timestamp".log
 
